930c006d856ec8c1b3759f5be9d03a26b7da9d6f
Muzaffar khan <41264014+muzafferjoya@users.noreply.github.com>
2022-02-14 11:59:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,22 @@
-provider "aws" {
-  region                  = "us-east-1"
-  shared_credentials_file = "~/.aws/credentials"
-  profile                 = "terraform"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
 }
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+#provider "aws" {
+#  region                  = "us-east-1"
+#  shared_credentials_file = "~/.aws/credentials"
+#  profile                 = "terraform"
+#}
 
 resource "aws_s3_bucket" "mywb"{
     bucket = "website-code-mk"
